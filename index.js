2bdd8507b88154dc025ba1ea1c811388369629ca
Ravindra Marella <mv.ravindra007@gmail.com>
2021-06-20 20:25:57 +0530

    Fix `EPERM` on Windows using `0o700`

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 #!/usr/bin/env node
 
-import { rm } from 'node:fs/promises';
+import * as fs from 'node:fs/promises';
 
-const del = async (path) => rm(path, { recursive: true, force: true });
+const del = async (path) => {
+  try {
+    await rmrf(path);
+  } catch (e) {
+    // Windows throws EPERM for write-protected directories.
+    if (e.code !== 'EPERM') {
+      throw e;
+    }
+    await fixPermissions(path);
+    await rmrf(path);
+  }
+};
+
+const rmrf = async (path) => fs.rm(path, { recursive: true, force: true });
+
+const fixPermissions = async (path) => {
+  try {
+    await fs.chmod(path, 0o700);
+  } catch (e) {
+    if (e.code !== 'ENOENT') {
+      throw e;
+    }
+  }
+};
 
 const delAll = async (paths) => Promise.all(paths.map(del));
 
